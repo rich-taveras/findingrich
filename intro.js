d8a240239a128bab4ea624a8217f442d6cf8f0f2
rich-taveras <rtaveras@yahoo.com>
2023-08-06 15:26:42 -0400

    added to HTML

--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const introContainer = document.querySelector(".intro-container");
-    const hiddenContainer = document.querySelector(".hidden-container");
+    var introContainer = document.querySelector(".intro-container");
+    var hiddenContainer = document.querySelector(".hidden-container");
 
     // Show the hidden-container after 2 seconds
     setTimeout(function() {
